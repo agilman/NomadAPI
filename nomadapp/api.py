df55dedf4f018dd712b446e8ae0d6215cc5d1077
agilman <gilmanalex@gmail.com>
2020-10-03 18:16:30 -0700

    Created a model for adventures. Created a route for adventures post requests.

--- nomadapp/api.py
+++ nomadapp/api.py
@@ -24,3 +24,35 @@ def user(request,userName=None):
         response = HttpResponse()
         response['allow'] = ','.join(['get','options'])
         return response
+
+@csrf_exempt
+def adventures(request,advId=None):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        userId = int(data["user"])
+        print("Hello", userId)
+        #TODO: VALIDATION
+        user = User.objects.get(pk=userId)
+
+        advName = data["advName"]
+        advType = data["advType"]
+        advStatus = data["advStatus"]
+
+        #TODO
+        #If advStatus = active, need to unset previous active.
+
+        adv = Adventure(name=advName,user=user,advType=advType,advStatus=advStatus)
+        adv.save()
+
+        #create directory
+        #media_root = settings.USER_MEDIA_ROOT
+        #os.mkdir(media_root + "/" + str(userId)+"/"+str(adv.id))
+        #os.mkdir(media_root + "/" + str(userId)+"/"+str(adv.id)+"/gear")
+
+        serialized = AdventureSerializer(adv)
+        return JsonResponse(serialized.data,safe=False)
+
+    if request.method == 'OPTIONS':
+        response = HttpResponse()
+        response['allow'] = ','.join(['get','options'])
+        return response
--- nomadapp/models.py
+++ nomadapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+from django.conf import settings
+
 # Create your models here.
+class Adventure(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE,)
+    name = models.CharField(max_length=32)
+    advType = models.IntegerField()
+    advStatus = models.IntegerField()
