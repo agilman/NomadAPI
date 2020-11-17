e175f3ef6f2c11e855b322d9f5640bbb6bd720ae
agilman <gilmanalex@gmail.com>
2020-11-17 13:30:37 -0800

    Creating a directory for user upon registration. Creating a directory for media upon adventure creation. creating a directory for media upon map creation. saving media to directory upon upload!

--- nomadapp/views.py
+++ nomadapp/views.py
@@ -1,6 +1,7 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.models import User
+import os
 
 from rest_framework.parsers import JSONParser
 
@@ -12,7 +13,7 @@ def registration(request):
     if request.method == 'POST':
         data = JSONParser().parse(request)
 
-        #TODO : Validate data!!!
+        # TODO: Validate data!!!
         nu = {
             'username' : data['username'],
             'email' : data['email'],
@@ -23,6 +24,11 @@ def registration(request):
         newUser = User.objects.create_user(nu['username'],nu['email'],nu['password'])
         newUser.save()
 
+        #create directory for user media
+        target = os.path.join(settings.USER_MEDIA_ROOT, str(newUser.id))
+        target2 = os.path.join(target ,'profile_pictures')
+        os.mkdir(target)
+        os.mkdir(target2)
         return JsonResponse(UserSerializer(newUser).data,safe=False)
 
     elif request.method == 'OPTIONS':
